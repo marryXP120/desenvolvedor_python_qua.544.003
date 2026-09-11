aa2f34cc02dd915f20ee071ac924bc225e7e5f8d
marryXP120 <socorromilanez@gmail.com>
2026-09-10 21:49:39 -0300

    Aula do dia 10/09/2026

---
+++ Parte07_Flask/01_meu_primeiro_app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    nome = None
+    if request.method == "POST":
+        nome = request.form.get("nome")
+    return render_template("index.html", nome=nome)
+
+@app.route("/novaPagina")
+def nova_pagina():
+    return render_template("segunda-pagina.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
