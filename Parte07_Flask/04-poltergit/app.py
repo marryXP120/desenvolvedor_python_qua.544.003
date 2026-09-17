e2311252164c80a4008253a9c9a12278eac3c7e1
marryXP120 <socorromilanez@gmail.com>
2026-09-16 21:24:11 -0300

    Aula do dia 16092026

--- Parte07_Flask/04-poltergit/app.py
+++ Parte07_Flask/04-poltergit/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import pyautogui as auto
 
-from dateime import date 
+from datetime import date 
 
 app = Flask(__name__)
 
@@ -17,17 +17,25 @@ def comitar():
     repositorio = None
     if request.method == 'POST':
         repositorio = request.form['repositorio']
+    if repositorio:
+        auto.PAUSE = 1
+        auto.hotkey('win', 'r')
+        auto.write('cmd')
+        auto.press('enter')
+        auto.write(f"cd {repositorio}")
+        auto.press('enter')
+        auto.write("git add .")
+        auto.press('enter')
+        auto.write(f'git commit -m "Commit do dia {hoje}"')
+        auto.press('enter')
+        auto.sleep(3)
+        auto.write("exit")
+        auto.press('enter')
+        return render_template("index.html")
+    else:
+        msg = "Repositorio invalido."
     return render_template('index.html')
-    auto.PAUSE = 1
-    auto.hotkey('win', 'r')
-    auto.write('cmd')
-    auto.press('enter')
-    auto.write(f"cd {repositorio}")"
-    auto.press('enter')
-else:
-    msg = "Repositorio invalido.
-    return render_template("index.html")"
-    return render_template('index.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
